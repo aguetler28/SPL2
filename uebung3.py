0332d226f14ede1ec71e86146235e117f14ec033
aguetler28 <aguetler28@icapital.at>
2018-06-11 06:54:45 -0700

    Wuerfeln erweiter

--- uebung3.py
+++ uebung3.py
@@ -1,15 +1,16 @@
 # uebung3.py
 import random
 zahlen = []
-
-for i in range (0,10):
+anzahlWuerfe = input("Wie oft soll gewürfelt werden? ")
+anzahlWuerfe = int(anzahlWuerfe)
+for i in range (0,anzahlWuerfe):
     wurf = random.randint(1,6) 
     zahlen.append(wurf)
-    print(wurf, end="...")
+    # print(wurf, end="...")
 
 print ()
 print ("Ergebnis: ")
-print (zahlen)
+# print (zahlen)
 
 for i in range(1,7):
     print (i,"er :", zahlen.count(i))
